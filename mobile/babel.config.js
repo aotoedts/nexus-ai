9ff5e6bd68feb73a422866b413b14bfb86090e42
aotoedts <aotoedts@example.com>
2026-08-07 20:26:19 -0300

    fix: adiciona plugins do babel para private class fields (compat Hermes)

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,4 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
-  return { presets: ['babel-preset-expo'] };
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-transform-private-methods',
+      '@babel/plugin-transform-class-properties',
+    ],
+  };
 };
